853ac847f8a25fceeced2710d897752799bb890b
kkoouuya0730 <kouya.0730@icloud.com>
2026-05-30 19:48:07 +0900

    PostModelのテスト実装

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "titleがあれば有効" do
+    post = Post.new(
+      title: "サンプル",
+      body: "sample"
+    )
+    assert post.valid?
+  end
+
+  test "titleがなければ有効" do
+    post = Post.new(
+      title: nil,
+      body: "sample"
+    )
+    assert_not post.valid?
+    assert_includes post.errors[:title], "can't be blank"
+  end
 end
